dfc863b63595e0d9cb65de7ba78c1037ac48b1cc
rohanmr <maindarkarrohan11@gmail.com>
2026-09-06 23:01:35 +0530

    update cleanup code

--- src/app.js
+++ src/app.js
@@ -28,10 +28,7 @@ const PORT = process.env.PORT || 5000;
 //   })
 // );
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://127.0.0.1:5173",
   "http://192.168.1.11:5173",
-  "http://10.203.34.84:5173",
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
